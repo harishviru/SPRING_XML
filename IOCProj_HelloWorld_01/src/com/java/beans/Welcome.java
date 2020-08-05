460a0fcd7647f8cb77583831e8e3338aa2bf6f8b
harishviru <57554695+harishviru@users.noreply.github.com>
2020-08-05 19:05:18 +0530

    Changes

--- IOCProj_HelloWorld_01/src/com/java/beans/Welcome.java
+++ IOCProj_HelloWorld_01/src/com/java/beans/Welcome.java
@@ -3,8 +3,13 @@ package com.java.beans;
 
 public class Welcome {
 
+	
+	public Welcome() {
+    System.out.println("Welcome Construtor......!");
+	}
+	
 	public void sayHello(){
-		System.out.println("Welcome to Spring XML World.................##!");
+		System.out.println("Welcome to Spring XML World................!");
 	}
 	
 	
