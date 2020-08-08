23121cceedcc2ae37a760166651099f854d3166c
harishviru <57554695+harishviru@users.noreply.github.com>
2020-08-08 15:20:59 +0530

    changes

--- IOCProj_06_ConstructorInjection_NonPrimitiveOrReferenceType/src/com/java/test/ConstructorInjectionTest.java
+++ IOCProj_06_ConstructorInjection_NonPrimitiveOrReferenceType/src/com/java/test/ConstructorInjectionTest.java
@@ -9,15 +9,19 @@ import com.java.beans.Car;
 public class ConstructorInjectionTest {
 
 	public static void main(String[] args) {
-		//Create IOC Container & load XML File into Container
-		ApplicationContext context=new ClassPathXmlApplicationContext("com/java/cfgs/ConstructorInjection.xml");
+		// Create IOC Container & load XML File into Container
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/java/cfgs/ConstructorInjection.xml");
+
+		// get CAR object from IOC Container
+		Car car = context.getBean("car", Car.class);
+
+		// call printCarData
+		car.printCarData();
+         //getting default beanIds
+		for(String beanId:context.getBeanDefinitionNames()){
+			System.out.println("beanId--->"+beanId);
+		}
 		
-		//get CAR object from IOC Container
-		Car car  = context.getBean("car", Car.class);
-		
-		//call  
-        car.printCarData();
-	
 	}
 
 	
